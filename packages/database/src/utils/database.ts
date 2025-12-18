d34e95eaee7c9723febb1f8ac59bd83bfc1ad1c1
ridafkih <hello@rida.dev>
2025-12-18 18:26:50 -0500

    fix(database): use bun-sql

--- packages/database/src/utils/database.ts
+++ packages/database/src/utils/database.ts
@@ -1,4 +1,4 @@
 import env from "@keeper.sh/env/database";
-import { drizzle } from "drizzle-orm/singlestore";
+import { drizzle } from "drizzle-orm/bun-sql";
 
 export const database = drizzle(env.DATABASE_URL);
