aae5d2d6ee5c6faf8e53272e3fefbe7b197d6fa8
ridafkih <hello@rida.dev>
2025-12-18 18:06:48 -0500

    fix(database): export database from module

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import env from "@keeper.sh/env/database";
 
-const database = drizzle(env.DATABASE_URL);
+export const database = drizzle(env.DATABASE_URL);
