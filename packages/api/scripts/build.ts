2755d35604ac64e5738639ad42799bd772aeb344
ridafkih <hello@rida.dev>
2025-12-26 19:29:10 -0700

    feat(cron, api): for dynamic entry points, use bun scripts to build

---
+++ packages/api/scripts/build.ts
@@ -0,0 +1,16 @@
+import { build, Glob } from "bun";
+
+const ROUTES_GLOB = new Glob("src/routes/**/*.ts");
+const ENTRY_POINT_GLOB = new Glob("src/index.ts");
+
+const entrypoints = Array.from([
+  ...ROUTES_GLOB.scanSync(),
+  ...ENTRY_POINT_GLOB.scanSync(),
+]);
+
+build({
+  entrypoints,
+  outdir: "./dist",
+  root: "src",
+  target: "bun",
+});
