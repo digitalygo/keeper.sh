902df88c14c6a1c6005d68e4898b9279bf1ff6b9
ridafkih <hello@rida.dev>
2026-01-22 00:52:13 -0500

    fix(next-web): inconsistent subtext sizing

--- applications/next-web/src/compositions/auth-form/components/auth-form-footer.tsx
+++ applications/next-web/src/compositions/auth-form/components/auth-form-footer.tsx
@@ -1,10 +1,10 @@
 import type { FC, PropsWithChildren } from "react"
-import { MicroCopy } from "@/components/micro-copy"
+import { Copy } from "@/components/copy"
 
 export const AuthFormFooter: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <MicroCopy className="text-center">
+    <Copy className="text-center">
       {children}
-    </MicroCopy>
+    </Copy>
   )
 }
